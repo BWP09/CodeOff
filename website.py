26e39a4c8e1721e99d9b2c2b9815f6469a897936
BWP09 <bwpayne2@gmail.com>
2023-09-08 07:30:39 -0700

    update

--- website.py
+++ website.py
@@ -12,28 +12,7 @@ def favicon():
 def index():
     if fl.request.method == "POST":
         long_url = fl.request.form["long_url"]
-        short_url = generate_random_id()
 
-        if long_url in shortened_urls.values():
-            short_url = list(shortened_urls.keys())[list(shortened_urls.values()).index(long_url)]
-
-        else:
-            while short_url in shortened_urls:
-                short_url = generate_random_id()
-        
-            shortened_urls[short_url] = long_url
-            url_data[short_url] = {}
-            url_data[short_url]["counter"] = 0
-            url_data[short_url]["date"] = datetime.datetime.today().date().strftime("%m/%d/%Y")
-        
-        save_jsons()
-
-        return fl.render_template("shortened.html",
-                                  base_url = base_url,
-                                  short_url = short_url,
-                                  long_url_full = long_url,
-                                  long_url = long_url[:40] + "..." if len(long_url) > 40 else long_url) # f"Shortened URL: {fl.request.root_url}{short_url}"
-    
     return fl.render_template("index.html")
 
 if __name__ == "__main__":
